d9a1d43ec7fa856d77fe01d1964cc82d3868f339
joyinfant99 <joyinfant99@gmail.com>
2024-07-03 18:55:10 +0200

    Prepare backend for deployment

--- huemagik-backend/app.py
+++ huemagik-backend/app.py
@@ -1,13 +1,13 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 import numpy as np
 from sklearn.cluster import KMeans
-import io
 import traceback
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "https://yourusername.github.io"}})
 
 def get_colors(image, number_of_colors):
     try:
@@ -66,4 +66,5 @@ def test():
     return jsonify({'message': 'Backend is working!'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
